461bf27d7dbc612268e5953b98ac7676e60c43a4
Illia kalinin <illinoiz4305@gmail.com>
2022-10-29 16:40:09 +0300

    Add variables

---
+++ js/index.js
@@ -0,0 +1,31 @@
+// Виведення в консоль
+console.log("Hello, World!");
+
+// Вивести в модальне вікно
+// alert("Hello, world!");
+
+/*
+Багатостроковий коментар
+*/
+
+// Змінні
+
+let a = 1;
+console.log("a =", a);
+a = a + 1;
+console.log("a =", a);
+
+const pi = 3.14; // не можна змінювати значення!
+console.log(pi);
+
+var b = 10;
+console.log("b =", b);
+
+// стиль іменування camelCase
+// lowerCamelcase
+// upperCamelCase
+
+// PI,MATH_PI
+
+// 1    a-z, A-Z, _, $
+// 2    a-z, A-Z, _, $, 0-9
